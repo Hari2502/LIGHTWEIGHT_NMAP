c419a9f8abef31b084d7167e0863c4a959404809
Harikrishna Patel <69990964+Hari2502@users.noreply.github.com>
2023-05-18 16:02:10 -0400

    Create target_file_scan.py

---
+++ target_file_scan.py
@@ -0,0 +1,22 @@
+import nmap
+
+input_file = input("Enter the path to the file containing target IP addresses or hostnames: ")
+
+with open(input_file, 'r') as file:
+    target_hosts = file.read().splitlines()
+
+target_ports = input("Enter the target ports (comma-separated): ").split(',')
+
+nm = nmap.PortScanner()
+
+for target_host in target_hosts:
+    nm.scan(target_host, ','.join(target_ports))
+    for host in nm.all_hosts():
+        print(f"Host: {host}")
+        print(f"State: {nm[host].state()}")
+        for proto in nm[host].all_protocols():
+            print(f"\nProtocol: {proto}")
+            ports = nm[host][proto].keys()
+            for port in ports:
+                print(f"Port: {port}\tState: {nm[host][proto][port]['state']}")
+        print()
