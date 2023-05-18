02107d26d71334e8d25857bf4f7d7af0cc228647
Harikrishna Patel <69990964+Hari2502@users.noreply.github.com>
2023-05-18 16:00:03 -0400

    Create udp_port_scanner.py

---
+++ udp_port_scanner.py
@@ -0,0 +1,16 @@
+import nmap
+
+target_host = input("Enter the target IP address or hostname: ")
+target_ports = input("Enter the target ports (comma-separated): ").split(',')
+
+nm = nmap.PortScanner()
+nm.scan(target_host, ','.join(target_ports), arguments='-sU')
+
+for host in nm.all_hosts():
+    print(f"Host: {host}")
+    print(f"State: {nm[host].state()}")
+    for proto in nm[host].all_protocols():
+        print(f"\nProtocol: {proto}")
+        ports = nm[host][proto].keys()
+        for port in ports:
+            print(f"Port: {port}\tState: {nm[host][proto][port]['state']}")
